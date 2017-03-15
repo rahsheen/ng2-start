8b11ed683fdbb239de1d5eb576e6567a693f5b1b
R. Anthony Porter <rah@sheenonline.biz>
2017-03-15 11:14:04 -0400

    Moved app to dist folder. Setup basic user-management interface.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,70 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <div class="jumbotron">
-            <h1>Welcome a el this App!</h1>
+    <header>
+        <nav class="navbar navbar-inverse">
+            <div class="navbar-header">
+                <a href="/" class="navbar-brand">My Angular 2 App!</a>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+
+    <div class="row">
+        <div class="col-sm-4">
+            <div *ngIf="users">
+                <ul class="list-group users-list">
+                    <li class="list-group-item" 
+                    *ngFor="let user of users"
+                    (click)="selectUser(user)"
+                    [class.active]="user === activeUser">
+                        {{ user.name }} ({{ user.username }})
+                    </li>
+                </ul>
+            </div>
         </div>
+        <div class="col-sm-8">
+
+            <div class="jumbotron" *ngIf="activeUser">
+                <h2>{{ activeUser.name }} <small>{{ activeUser.username }}</small></h2>
+            </div>
+
+            <div class="jumbotron" *ngIf="!activeUser">
+                <span class="glyphicon glyphicon-hand-left"></span>
+                <h2>Choose a User</h2>
+            </div>
+        </div>
+    </div>
+     </main>
+
+    <footer class="text-center">
+        Copyright &copy; 2017
+    </footer>
     `,
     styles: [`
-    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
+    .users-list li {
+        cursor: pointer;
+    }
+
+    .jumbotron .glyphicon {
+        font-size: 80px;
+    }
     `]
 })
 
-export class AppComponent {}
+export class AppComponent {
+    message: string = "Hello!";
+    users = [
+        {id: 25, name: 'Rahsheen',username: 'rahsheen'},
+        {id: 26, name: 'Nick', username: 'whatnicktweets'},
+        {id: 27, name: 'Holly', username: 'hollylawly'}
+    ];
+
+    activeUser;
+
+    selectUser(user) {
+        this.activeUser = user;
+    }
+
+}
---
+++ dist/app.component.js
@@ -0,0 +1,32 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var AppComponent = (function () {
+    function AppComponent() {
+        this.message = "Hello!";
+        this.users = [
+            { id: 25, name: 'Rahsheen', username: 'rahsheen' },
+            { id: 26, name: 'Nick', username: 'whatnicktweets' },
+            { id: 27, name: 'Holly', username: 'hollylawly' }
+        ];
+    }
+    AppComponent.prototype.selectUser = function (user) {
+        this.activeUser = user;
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'my-app',
+        template: "\n    <header>\n        <nav class=\"navbar navbar-inverse\">\n            <div class=\"navbar-header\">\n                <a href=\"/\" class=\"navbar-brand\">My Angular 2 App!</a>\n            </div>\n        </nav>\n    </header>\n\n    <main>\n\n    <div class=\"row\">\n        <div class=\"col-sm-4\">\n            <div *ngIf=\"users\">\n                <ul class=\"list-group users-list\">\n                    <li class=\"list-group-item\" \n                    *ngFor=\"let user of users\"\n                    (click)=\"selectUser(user)\"\n                    [class.active]=\"user === activeUser\">\n                        {{ user.name }} ({{ user.username }})\n                    </li>\n                </ul>\n            </div>\n        </div>\n        <div class=\"col-sm-8\">\n\n            <div class=\"jumbotron\" *ngIf=\"activeUser\">\n                <h2>{{ activeUser.name }} <small>{{ activeUser.username }}</small></h2>\n            </div>\n\n            <div class=\"jumbotron\" *ngIf=\"!activeUser\">\n                <span class=\"glyphicon glyphicon-hand-left\"></span>\n                <h2>Choose a User</h2>\n            </div>\n        </div>\n    </div>\n     </main>\n\n    <footer class=\"text-center\">\n        Copyright &copy; 2017\n    </footer>\n    ",
+        styles: ["\n    .users-list li {\n        cursor: pointer;\n    }\n\n    .jumbotron .glyphicon {\n        font-size: 80px;\n    }\n    "]
+    })
+], AppComponent);
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
